886cd6428cf11cbeaa9ec1b62a07e35baacb4b6f
KaminskyVova <kamok@bk.ru>
2020-03-28 22:47:03 +0300

    3d-glo

---
+++ src/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+import siteTimer from './modules/siteTimer';
+import siteMenu from './modules/siteMenu';
+import sitePopup from './modules/sitePopup';
+import siteTabs from './modules/siteTabs';
+import siteSlider from './modules/siteSlider';
+import chengeFoto from './modules/chengeFoto';
+import priceCalc from './modules/priceCalc';
+import sendFormAjax from './modules/sendFormAjax';
+
+import "@babel/polyfill";
+import 'nodelist-foreach-polyfill';
+
+const elementClosest = require('element-closest');
+ 
+elementClosest(window);
+
+var Promise = require('es6-promise').Promise;
+
+
+
+
+//таймер отчета
+siteTimer();
+//Меню сайта
+siteMenu();
+//popUp
+sitePopup();
+//табы
+siteTabs();
+// слайдер
+siteSlider();
+//Наша команда смена фото при навидении
+chengeFoto();
+//Расчет стоимости
+priceCalc();
+// отправка формы ajax
+sendFormAjax();
